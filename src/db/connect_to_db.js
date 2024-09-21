ec99b1397b16d8d7a3d243481d582fc8a35aa583
sameerkumar2704 <shivamjnv01@gmail.com>
2024-09-21 08:26:15 +0530

    db-connection

---
+++ src/db/connect_to_db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants.js';
+
+( async ()=> {
+    try{
+       const mongoDBConnetionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB Connected DB HOST : ${mongoDBConnetionInstance.connection.host}`)
+    } catch(error){
+        console.log("MogoDB Connection Error 🍀: " , error)
+        process.exit(1)
+    }
+})()
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import dotenv from 'dotenv'
+import './db/connect_to_db.js'
+
+dotenv.config({path:"./env"})
